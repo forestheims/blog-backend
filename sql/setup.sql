49425e48bb602ed0a16e14dc0197254998d8e4a7
forestheims <forestheims@gmail.com>
2022-12-22 21:09:28 -0800

    add pre seeded links to database for now

--- sql/setup.sql
+++ sql/setup.sql
@@ -5,7 +5,7 @@ CREATE TABLE links (
   link_url VARCHAR UNIQUE NOT NULL,
   title VARCHAR,
   tags VARCHAR ARRAY,
-  order_index INTEGER DEFAULT 0,
+  order_index DATE DEFAULT CURRENT_DATE;
 );
 
 INSERT INTO links
@@ -13,6 +13,14 @@ INSERT INTO links
 VALUES
   ('https://www.postgresql.org/', 'PostgreSQL', ARRAY['database', 'open source']),
   ('https://www.postgresql.org/docs/current/index.html', 'PostgreSQL Documentation', ARRAY['documentation', 'open source']),
-  ('https://docs.github.com/en/actions/automating-builds-and-tests/building-and-testing-nodejs', 'Building and Testing Node.js - GitHub', ARRAY['YAML', 'GitHub']);
+  ('https://docs.github.com/en/actions/automating-builds-and-tests/building-and-testing-nodejs', 'Building and Testing Node.js - GitHub', ARRAY['YAML', 'GitHub']),
+  ('https://reactrouter.com/en/main','React Router', ARRAY['routing']),
+  ('https://beta.reactjs.org/learn/thinking-in-react','Thinking in React', ARRAY['component', 'documentation']),
+  ('https://jestjs.io/docs/setup-teardown','Setup and Teardown | Jest', ARRAY['testing', 'documentation']),
+  ('https://developer.mozilla.org/en-US/docs/Web/JavaScript','JavaScript | MDN', ARRAY['JavaScript', 'documentation']),
+  ('https://javascript.info/','The Modern JavaScript Tutorial', ARRAY['JavaScript', 'documentation']),
+  ('https://html.spec.whatwg.org/dev/common-microsyntaxes.html','HTML Standard, Edition for Web Developers', ARRAY['HTML', 'documentation']),
+  ('https://jackschaedler.github.io/circles-sines-signals/','Circles Sines and Signals', ARRAY['audio engineering', 'signal processing']),
+  ('https://ryanstutorials.net/problem-solving-skills/','Problem Solving Skills and Techniques', ARRAY['problem solving', 'tutorials']);
 
 
