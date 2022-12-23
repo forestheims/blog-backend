c3540bae549f20ac99a589eaebce0b0985a535ee
forestheims <forestheims@gmail.com>
2022-12-22 16:02:59 -0800

    update setup.sql with order_index and insert a few links

--- sql/setup.sql
+++ sql/setup.sql
@@ -4,5 +4,15 @@ CREATE TABLE links (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   link_url VARCHAR UNIQUE NOT NULL,
   title VARCHAR,
-  tags VARCHAR ARRAY
+  tags VARCHAR ARRAY,
+  order_index INTEGER DEFAULT 0,
 );
+
+INSERT INTO links
+  (link_url, title, tags)
+VALUES
+  ('https://www.postgresql.org/', 'PostgreSQL', ARRAY['database', 'open source']),
+  ('https://www.postgresql.org/docs/current/index.html', 'PostgreSQL Documentation', ARRAY['documentation', 'open source']),
+  ('https://docs.github.com/en/actions/automating-builds-and-tests/building-and-testing-nodejs', 'Building and Testing Node.js - GitHub', ARRAY['YAML', 'GitHub']);
+
+
